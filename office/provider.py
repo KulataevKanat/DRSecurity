6d7b125409d7ed6ea827ccf99bcf63188e209979
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-12-06 04:27:53 +0600

    added password claims refactored

--- office/provider.py
+++ office/provider.py
@@ -7,11 +7,16 @@ def generate_access_token(user):
     access_token_payload = {
         'user_id': user.id,
         'username': user.username,
+        'password': user.password,
         'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=5),
         'iat': datetime.datetime.utcnow(),
     }
-    access_token = jwt.encode(access_token_payload,
-                              settings.SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+    access_token = jwt.encode(
+        access_token_payload,
+        settings.SECRET_KEY,
+        algorithm='HS256').decode('utf-8')
+
     return access_token
 
 
@@ -22,7 +27,10 @@ def generate_refresh_token(user):
         'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7),
         'iat': datetime.datetime.utcnow()
     }
+
     refresh_token = jwt.encode(
-        refresh_token_payload, settings.REFRESH_TOKEN_SECRET, algorithm='HS256').decode('utf-8')
+        refresh_token_payload,
+        settings.REFRESH_TOKEN_SECRET,
+        algorithm='HS256').decode('utf-8')
 
     return refresh_token
