310d05853d6b53957753dba1e34229f64dac61d3
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-12-21 03:46:52 +0600

    Added Group Serializer

--- office/serializers.py
+++ office/serializers.py
@@ -1,9 +1,18 @@
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 from office.models import User, Table
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
+
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer()
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password',
