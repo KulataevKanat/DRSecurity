8c85d14fbf59517e4c1d75b07a902494f29dc044
KulataevKanat <RaketaEnot4466@gmail.com>
2021-06-13 03:24:10 +0600

    updated on the path added swagger urls

--- DrJwt/urls.py
+++ DrJwt/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from office import urls as office_urls
+from DrJwt.swagger import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('office/', include(office_urls)),
-
-    url('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('office/', include(office_urls)),
 ]
+
+urlpatterns += doc_urls
