69a5b84505e15cddb05ebe0be6dbb5e5adae74a7
KulataevKanat <RaketaEnot4466@gmail.com>
2021-06-13 03:55:04 +0600

    Table views

---
+++ office/views/TableViews.py
@@ -0,0 +1,37 @@
+from rest_framework import generics
+
+from office.models import Table
+from office.serializers import TableSerializers
+
+
+class CreateTableView(generics.CreateAPIView):
+    """Добавление стола"""
+
+    serializer_class = TableSerializers.AllTableSerializer
+
+
+class DeleteTableByIdView(generics.DestroyAPIView):
+    """Удаление стола по идентификации"""
+
+    queryset = Table.objects.all()
+
+
+class UpdateTableByIdView(generics.UpdateAPIView):
+    """Обновление стола по идентификации"""
+
+    queryset = Table.objects.all()
+    serializer_class = TableSerializers.AllTableSerializer
+
+
+class GetTableView(generics.ListAPIView):
+    """Вывод столов"""
+
+    queryset = Table.objects.all()
+    serializer_class = TableSerializers.AllTableSerializer
+
+
+class FindTableByIdView(generics.RetrieveAPIView):
+    """Поиск стола по идентификации"""
+
+    queryset = Table.objects.all()
+    serializer_class = TableSerializers.AllTableSerializer
